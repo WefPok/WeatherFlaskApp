f8c622496eb5b8c67adce3bde4bcaccf08faa6f6
WefPok <nik4zerg@gmail.com>
2024-02-14 14:22:21 +0300

    Coms and readme

--- user.py
+++ user.py
@@ -3,12 +3,25 @@ import time
 
 class User:
     def __init__(self, id, username, balance):
+        """
+        Initialize a new User instance.
+
+        :param id: The unique identifier for the user.
+        :param username: The username of the user.
+        :param balance: The current balance of the user's account.
+        """
         self.id = id
         self.username = username
         self.balance = balance
 
     @staticmethod
     def get_user_by_id(user_id):
+        """
+        Retrieve a user from the database by their user ID.
+
+        :param user_id: The ID of the user to retrieve.
+        :return: A User instance if found, None otherwise.
+        """
         with sqlite3.connect('users.db') as conn:
             c = conn.cursor()
             c.execute('SELECT * FROM users WHERE id = ?', (user_id,))
@@ -19,6 +32,13 @@ class User:
 
     @staticmethod
     def add_user(username, balance):
+        """
+        Add a new user to the database.
+
+        :param username: The username of the new user.
+        :param balance: The starting balance of the new user.
+        :return: The ID of the newly created user.
+        """
         with sqlite3.connect('users.db') as conn:
             c = conn.cursor()
             c.execute('INSERT INTO users (username, balance) VALUES (?, ?)', (username, balance))
@@ -27,6 +47,13 @@ class User:
 
     @staticmethod
     def delete_user(user_id):
+        """
+        Delete a user from the database by their user ID.
+
+        :param user_id: The ID of the user to delete.
+        :return: The number of rows affected (should be 1 if a user was deleted).
+        """
+
         with sqlite3.connect('users.db') as conn:
             c = conn.cursor()
             c.execute('DELETE FROM users WHERE id = ?', (user_id,))
@@ -34,6 +61,15 @@ class User:
             return c.rowcount  # Return the number of rows affected
 
     def update_balance(self, amount):
+        """
+        Update the balance of the user. If the update fails due to a database lock, it will retry the update a
+        maximum number of times before raising an exception.
+
+        :param amount: The amount to add to the user's balance. Can be negative.
+        :return: True if the update was successful, False if the update would result in a negative balance.
+        :raises: sqlite3.OperationalError if all retries fail due to a database lock.
+        """
+
         max_retries = 5  # Maximum number of retries
         retry_wait = 0.5  # Wait time between retries in seconds
         for attempt in range(max_retries):
